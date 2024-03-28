3edfb9b216d94f5791e77857ece321c286066bb0
mar bautista <marilirulita@gmail.com>
2024-03-27 18:52:08 -0700

    add customers fetch data in customers page to display customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,33 @@
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+      </div>
+    </div>
+  );
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -133,3 +133,111 @@ export async function authenticate(
     throw error;
   }
 }
+
+// customers code
+const CustomersFormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
+
+const CreateCustomer = CustomersFormSchema.omit({ id: true, date: true });
+const UpdateCustomer = CustomersFormSchema.omit({ id: true, date: true });
+
+export type CustomerState = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createCustomer(prevState: CustomerState, formData: FormData) {
+  // Validate form fields using Zod
+ const validatedFields = CreateCustomer.safeParse({
+   customerId: formData.get('customerId'),
+   amount: formData.get('amount'),
+   status: formData.get('status'),
+ });
+
+ // If form validation fails, return errors early. Otherwise, continue.
+ if (!validatedFields.success) {
+   return {
+     errors: validatedFields.error.flatten().fieldErrors,
+     message: 'Missing Fields. Failed to Create Customer.',
+   };
+ }
+
+ // Prepare data for insertion into the database
+ const { customerId, amount, status } = validatedFields.data;
+ const amountInCents = amount * 100;
+ const date = new Date().toISOString().split('T')[0];
+
+ // Insert data into the database
+ try {
+   await sql`
+     INSERT INTO invoices (customer_id, amount, status, date)
+     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+   `;
+ } catch (error) {
+   return {
+     message: 'Database Error: Failed to Create Invoice.',
+   };
+ }
+
+ revalidatePath('/dashboard/invoices');
+ redirect('/dashboard/invoices');
+}
+
+export async function updateCustomer(id: string, prevState: State, formData: FormData) {
+ 
+ const validatedFields = UpdateInvoice.safeParse({
+   customerId: formData.get('customerId'),
+   amount: formData.get('amount'),
+   status: formData.get('status'),
+ });
+
+ if (!validatedFields.success) {
+   return {
+     errors: validatedFields.error.flatten().fieldErrors,
+     message: 'Missing Fields. Failed to Update Invoice.',
+   };
+ }
+
+ const { customerId, amount, status } = validatedFields.data;
+ const amountInCents = amount * 100;
+
+ try {
+   await sql`
+   UPDATE invoices
+   SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+   WHERE id = ${id}
+ `;
+ } catch (error) {
+   return {
+     message: 'Database Error: Failed to Update Invoice.',
+   };
+ }
+ 
+ revalidatePath('/dashboard/invoices');
+ redirect('/dashboard/invoices');
+}
+
+export async function deleteCustomer(id: string) {
+  try {
+    await sql`DELETE FROM customers WHERE id = ${id}`;
+    revalidatePath('/dashboard/customers');
+    return { message: 'Deleted Customer' };
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Customer.',
+    };
+  }
+}
